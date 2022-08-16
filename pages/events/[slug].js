f7fd103a98f3375d7a6130b9778a3c4d4463f987
VentsislavKostadinov <ventsislav.kostadinov@angloamerican.com>
2022-08-16 20:02:57 +0100

    first layout

---
+++ pages/events/[slug].js
@@ -0,0 +1,9 @@
+import Layout from "../../components/Layout";
+
+export default function EventPageId() {
+  return (
+    <Layout>
+      <h1>Event Page ID</h1>
+    </Layout>
+  );
+}
---
+++ pages/events/add.js
@@ -0,0 +1,9 @@
+import Layout from "../../components/Layout";
+
+export default function AddEvent() {
+    return (
+        <Layout title='add New Event'>
+            <h2>Add Event</h2>
+        </Layout>
+    )
+}
